110ae77cc0654db4bf2fefead14b37d30655f8b2
zhangxhbeta <zhangxhbeta@gmail.com>
2016-08-25 17:37:11 +0800

    将 svg 图表改为自适应宽度

--- src/main/resources/static/demo.js
+++ src/main/resources/static/demo.js
@@ -4,32 +4,39 @@
  * @author zhangxh
  */
 $(function () {
-  var boxHeight = 320, minDetailHeight = 30;
 
-  function layout() {
-    // 工具栏固定, 图表高度固定, 底栏footer高度固定, 剩余的就是信息详情的高度
-    var availableHeight = $(window).innerHeight() - $('#toolbar').outerHeight() - boxHeight - $('#footer').outerHeight();
-    availableHeight = availableHeight > minDetailHeight ? availableHeight : minDetailHeight;
-    $('#detail').outerHeight(availableHeight);
-  }
+  // 首先计算一下高度宽度
+  var aspectRatio = 3; // 图表高度宽度比例 1:3
+  var minDetailHeight = 30; // 详情区域最小高度 30px
+  var smallDevicesWidth = 767; // 小屏幕宽度, 超出认定为其他大小
 
-  // 计算一次, 然后绑定 resiz 事件处理
-  layout();
-  $( window ).resize(function() {
-    layout();
-  });
-
-  // 开始部分，准备svg画布
+  // 获取当前 chart 可用宽度, 然后根据比例算出可用高度
   var boxWidth, box = d3.select("#chart").node().getBoundingClientRect();
   if (!box.width) {
     boxWidth = box.width;
   } else {
     boxWidth = box.right - box.left;
   }
-  var svg = d3.select('#chart').append('svg').attr('width', boxWidth).attr('height', boxHeight).style('display', 'block');
-  d3.select('#version').html('v' + d3.version + ' chart size: ' + boxWidth + 'x' + boxHeight);
+  var boxHeight = boxWidth / aspectRatio;
 
-  // 获取大小，高度固定／宽度自适应100%
+  // 根据窗口高度布局一次详情表格高度, 然后绑定 resiz 事件处理
+  // 这里做了响应式, 如果是手机屏幕就不做自适应了
+  if ($(window).innerWidth() > 767) {
+    layout();
+    $( window ).resize(function() {
+      layout();
+    });
+  }
+
+  // 开始部分，准备svg画布
+  var svg = d3.select('#chart').append('svg')
+      .attr('width', '100%')
+      .attr('height', 'auto')
+      .attr('viewBox', '0 0 ' + boxWidth + " " + boxHeight)
+      .attr('preserveAspectRatio', 'xMid yMid none')
+      .style('display', 'block');
+  d3.select('#version').html('v' + d3.version);
+
   var margin = {
     top: 20,
     right: 20,
@@ -40,6 +47,7 @@ $(function () {
   // svg 画布已准备妥当，开始准备图表元素
   var height = boxHeight - margin.bottom - margin.top,
       width = boxWidth - margin.left - margin.right;
+
   var parseDate = d3.time.format("%d-%b-%y").parse;
   var x = d3.time.scale().range([0, width]);
   var y = d3.scale.linear().range([height, 0]);
@@ -86,4 +94,12 @@ $(function () {
         .style("text-anchor", "end")
         .text("价格 (¥)");
   });
+
+  // 详情部分布局
+  function layout() {
+    // 工具栏固定, 图表高度固定, 底栏footer高度固定, 剩余的就是信息详情的高度
+    var availableHeight = $(window).innerHeight() - $('#toolbar').outerHeight() - boxHeight - $('#footer').outerHeight();
+    availableHeight = availableHeight > minDetailHeight ? availableHeight : minDetailHeight;
+    $('#detail').outerHeight(availableHeight);
+  }
 });
