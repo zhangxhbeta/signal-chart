f458bfea8893dc87f0d70888a00a2cbebc65571f
zhangxhbeta <zhangxhbeta@gmail.com>
2016-08-26 11:17:26 +0800

    调整 ie 下面问题，ie8 始终有问题放弃之

--- src/main/resources/static/demo.js
+++ src/main/resources/static/demo.js
@@ -9,31 +9,23 @@ $(function () {
   var aspectRatio = 3; // 图表高度宽度比例 1:3
   var minDetailHeight = 30; // 详情区域最小高度 30px
   var smallDevicesWidth = 767; // 小屏幕宽度, 超出认定为其他大小
+  var ieFix = 1;
 
   // 获取当前 chart 可用宽度, 然后根据比例算出可用高度
-  var boxWidth, box = d3.select("#chart").node().getBoundingClientRect();
-  if (!box.width) {
-    boxWidth = box.width;
-  } else {
-    boxWidth = box.right - box.left;
-  }
-  var boxHeight = boxWidth / aspectRatio;
+  var size = calcChartSize();
 
   // 根据窗口高度布局一次详情表格高度, 然后绑定 resiz 事件处理
   // 这里做了响应式, 如果是手机屏幕就不做自适应了
   if ($(window).innerWidth() > 767) {
-    layout();
-    $( window ).resize(function() {
-      layout();
-    });
+    layoutDetail(size.height);
   }
 
   // 开始部分，准备svg画布
   var svg = d3.select('#chart').append('svg')
       .attr('width', '100%')
-      .attr('height', 'auto')
-      .attr('viewBox', '0 0 ' + boxWidth + " " + boxHeight)
-      .attr('preserveAspectRatio', 'xMid yMid none')
+      .attr('height', size.height)
+      .attr('viewBox', '0 0 ' + size.width + " " + size.height)
+      // .attr('preserveAspectRatio', 'none')
       .style('display', 'block');
   d3.select('#version').html('v' + d3.version);
 
@@ -45,8 +37,8 @@ $(function () {
   };
 
   // svg 画布已准备妥当，开始准备图表元素
-  var height = boxHeight - margin.bottom - margin.top,
-      width = boxWidth - margin.left - margin.right;
+  var height = size.height - margin.bottom - margin.top,
+      width = size.width - margin.left - margin.right;
 
   var parseDate = d3.time.format("%d-%b-%y").parse;
   var x = d3.time.scale().range([0, width]);
@@ -95,11 +87,37 @@ $(function () {
         .text("价格 (¥)");
   });
 
+  $( window ).resize(function() {
+    var size = calcChartSize();
+    $('#chart svg').height(size.height);
+
+    if ($(window).innerWidth() > 767) {
+      layoutDetail(size.height);
+    }
+  });
+
   // 详情部分布局
-  function layout() {
+  function layoutDetail(boxHeight) {
     // 工具栏固定, 图表高度固定, 底栏footer高度固定, 剩余的就是信息详情的高度
     var availableHeight = $(window).innerHeight() - $('#toolbar').outerHeight() - boxHeight - $('#footer').outerHeight();
     availableHeight = availableHeight > minDetailHeight ? availableHeight : minDetailHeight;
+
+    availableHeight -= ieFix; // ie 下面有点滚动条问题
+
     $('#detail').outerHeight(availableHeight);
+  }
+
+  function calcChartSize() {
+    var boxWidth, box = d3.select("#chart").node().getBoundingClientRect();
+    if (!box.width) {
+      boxWidth = box.width;
+    } else {
+      boxWidth = box.right - box.left;
+    }
+
+    return {
+      width: boxWidth,
+      height: Math.floor(boxWidth / aspectRatio)
+    }
   }
 });
