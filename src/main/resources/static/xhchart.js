af77384e01039daa4484f5c7ac994fd16433d028
zhangxhbeta <zhangxhbeta@gmail.com>
2016-09-01 16:25:59 +0800

    准备添加组件代码

---
+++ src/main/resources/static/xhchart.js
@@ -0,0 +1,20 @@
+/**
+ * 全局命名空间 xhchart, 目前里面有一个模块 signal, 用于机车图
+ *
+ * @author zhangxh
+ * @type {{signal}}
+ */
+(function () {
+  var xhchart = {};
+
+  /**
+   * 机车图模块
+   */
+  xhchart.signal = function () {
+
+    // 私有定义
+    var direction = 0;
+  };
+
+  this.xhchart = xhchart;
+}());
