c5d89e6583167392b50fe5f5d626114792d7bb40
zhangxhbeta <zhangxhbeta@gmail.com>
2016-09-09 12:11:53 +0800

    采用 webjar 和 require.js 方式来管理前端组件

---
+++ src/main/resources/static/assets/scripts/app.js
@@ -0,0 +1,11 @@
+requirejs.config({
+                   paths: {
+                     'xhchart' : 'xhchart'
+                   },
+                   shim: {
+                     xhchart: {
+                       exports: 'xhchart'
+                     }
+                   }
+
+                 });
